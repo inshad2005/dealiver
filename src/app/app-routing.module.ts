d1d77d344dadc9d2c0a167d82e2e4ec107d3a5c1
inshad2005 <promatics.inshad@gmail.com>
2017-11-18 00:31:24 +0530

    delaver

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,6 +10,7 @@ const routes: Routes = [
     { path: 'error', loadChildren: './server-error/server-error.module#ServerErrorModule' },
     { path: 'access-denied', loadChildren: './access-denied/access-denied.module#AccessDeniedModule' },
     { path: 'not-found', loadChildren: './not-found/not-found.module#NotFoundModule' },
+    { path: 'forgot-password', loadChildren: './forgot-password/forgot-password.module#ForgotPasswordModule' },
     { path: '**', redirectTo: 'not-found' }
 ];
 
